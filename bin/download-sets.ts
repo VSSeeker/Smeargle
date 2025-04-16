552b5a80a5fd57e3d84897988a9821caa538d8ea
Tycho Kaster <humanresourcesunofficial@proton.me>
2025-04-16 18:45:21 +0200

    TK set symbols

--- bin/download-sets.ts
+++ bin/download-sets.ts
@@ -3,16 +3,11 @@ import * as fs from "fs";
 import * as path from "path";
 import { cachePath } from "./paths";
 
-// Recommended to run first without, then run later with this if symbols are missing
-const USE_BULBAPEDIA_SYMBOLS = process.env.USE_BULBAPEDIA_SYMBOLS === "1";
-
 const setsPage = await fetch(
   "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_Trading_Card_Game_expansions",
 ).then(r => r.text());
-const symbolsPage = await fetch("https://cardmavin.com/pokemon/pokemon-card-set-symbols").then(r => r.text());
 
 const $sets = cheerioLoad(setsPage);
-const $symbols = cheerioLoad(symbolsPage);
 
 const locale = "en-US";
 
@@ -69,41 +64,15 @@ for (const row of setRows) {
     console.error(e);
   }
 
-  if (USE_BULBAPEDIA_SYMBOLS) {
-    const setSymbol = $sets(row).find("img").eq(0).attr("src");
-    const setSymbolDownloadUrl = convertMWThumb(setSymbol);
-    console.log(`${abbr} [${setSymbolDownloadUrl}] => ${setCachePath}`);
-    try {
-      const setLogoPath = path.join(setCachePath, `symbol.png`);
-      await writeCache(setSymbolDownloadUrl, setLogoPath);
-    } catch (e) {
-      console.error(e);
-    }
+  const setSymbol = $sets(row).find("img").eq(0).attr("src");
+  const setSymbolDownloadUrl = convertMWThumb(setSymbol);
+  console.log(`${abbr} [${setSymbolDownloadUrl}] => ${setCachePath}`);
+  try {
+    const setLogoPath = path.join(setCachePath, `symbol.png`);
+    await writeCache(setSymbolDownloadUrl, setLogoPath);
+  } catch (e) {
+    console.error(e);
   }
-}
-
-const symbolRows = $symbols("tbody tr");
-for (const row of symbolRows) {
-  let rowSet = $symbols(row).find("td").first().text().trim();
-  rowSet = rowSet
-    .replace(/ Base Set$/, "")
-    .replace(" and ", " & ")
-    .replace("Fire Red", "FireRed")
-    .replace("Leaf Green", "LeafGreen")
-    .replace("Pokemon", "Pokémon");
-
-  const rowSymbol = $symbols(row).find("td").last().find("img").attr("data-src");
-  const setAbbr = setCodes.get(rowSet);
-
-  if (!rowSymbol || !setAbbr) continue;
-  console.log(rowSet, setAbbr, rowSymbol);
-
-  const setCachePath = path.join(setsCachePath, setAbbr);
-  await fs.promises.mkdir(setCachePath, { recursive: true });
-
-  const setSymbolPath = path.join(setCachePath, `symbol.png`);
-
-  await writeCache(rowSymbol, setSymbolPath);
 }
 
 const promoBase = path.join(setsCachePath, "NP");
@@ -117,6 +86,46 @@ for (const set of promoSets) {
     console.log("Clone", set, inputFile, outputFile);
     await fs.promises.mkdir(promoPath, { recursive: true });
     await fs.promises.copyFile(inputFile, outputFile);
+  }
+}
+
+const tkSetSymbols = {
+  "TK5E":
+    "https://archives.bulbagarden.net/media/upload/thumb/1/1f/SetSymbolExcadrill_Half_Deck.png/30px-SetSymbolExcadrill_Half_Deck.png",
+  "TK5Z":
+    "https://archives.bulbagarden.net/media/upload/thumb/0/01/SetSymbolZoroark_Half_Deck.png/30px-SetSymbolZoroark_Half_Deck.png",
+  "TK6S":
+    "https://archives.bulbagarden.net/media/upload/thumb/4/46/SetSymbolSylveon_Half_Deck.png/30px-SetSymbolSylveon_Half_Deck.png",
+  "TK6N":
+    "https://archives.bulbagarden.net/media/upload/thumb/3/38/SetSymbolNoivern_Half_Deck.png/30px-SetSymbolNoivern_Half_Deck.png",
+  "TK7A":
+    "https://archives.bulbagarden.net/media/upload/thumb/a/a5/SetSymbolBisharp_Half_Deck.png/30px-SetSymbolBisharp_Half_Deck.png",
+  "TK7B":
+    "https://archives.bulbagarden.net/media/upload/thumb/3/3f/SetSymbolWigglytuff_Half_Deck.png/30px-SetSymbolWigglytuff_Half_Deck.png",
+  "TK8A":
+    "https://archives.bulbagarden.net/media/upload/thumb/c/cf/SetSymbolLatias_XY_Half_Deck.png/30px-SetSymbolLatias_XY_Half_Deck.png",
+  "TK8O":
+    "https://archives.bulbagarden.net/media/upload/thumb/8/8a/SetSymbolLatios_XY_Half_Deck.png/30px-SetSymbolLatios_XY_Half_Deck.png",
+  "TK9P":
+    "https://archives.bulbagarden.net/media/upload/thumb/7/79/SetSymbolPikachu_Libre_Half_Deck.png/30px-SetSymbolPikachu_Libre_Half_Deck.png",
+  "TK9S":
+    "https://archives.bulbagarden.net/media/upload/thumb/9/90/SetSymbolSuicune_Half_Deck.png/30px-SetSymbolSuicune_Half_Deck.png",
+  "TK10A":
+    "https://archives.bulbagarden.net/media/upload/thumb/b/ba/SetSymbolAlolan_Raichu_Half_Deck.png/30px-SetSymbolAlolan_Raichu_Half_Deck.png",
+  "TK10L":
+    "https://archives.bulbagarden.net/media/upload/thumb/a/a7/SetSymbolLycanroc_Half_Deck.png/30px-SetSymbolLycanroc_Half_Deck.png",
+};
+
+for (const [code, url] of Object.entries(tkSetSymbols)) {
+  const setCachePath = path.join(setsCachePath, code);
+  await fs.promises.mkdir(setCachePath, { recursive: true });
+  const setSymbolDownloadUrl = convertMWThumb(url);
+  console.log(`${code} [${setSymbolDownloadUrl}] => ${setCachePath}`);
+  try {
+    const setLogoPath = path.join(setCachePath, `symbol.png`);
+    await writeCache(setSymbolDownloadUrl, setLogoPath);
+  } catch (e) {
+    console.error(e);
   }
 }
 
